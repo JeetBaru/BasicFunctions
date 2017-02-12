8afb2da6407ff0073086f48960fff9b606db9a4a
Jeet Baru <jeet.baru@colorado.edu>
2017-02-12 13:04:53 -0700

    Chenages made to project1.c

--- project_1.c
+++ project_1.c
@@ -13,11 +13,12 @@ void test_data1()
 	uint8_t set_1[] = {0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF,0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF};
 	uint8_t * ptr = set_1;
 	uint32_t * pt = (uint32_t *)set_1;
+	printf("Test data 1\nInitial Data\n");
 	print_memory( ptr, 32);
-	printf("\n");
+	printf("\nData after Big to Little\n");
 	big_to_little32(pt,8);
 	print_memory( ptr, 32);
-	printf("\n");
+	printf("\nData After Little to Big\n");
 	little_to_big32(pt,8);
 	print_memory( ptr, 32);
 }
@@ -29,9 +30,10 @@ void test_data2()
 	uint8_t j;
 	uint8_t i=1;
 	uint8_t * dest=c;
-	uint32_t base=8;
+	uint32_t base=10;
 	uint32_t dat = 0;
 	uint32_t l=0;
+	printf("\n\n\nTest Data 2\n");
 	for(i=0;i<8;i++)
 	{
 		for(j=0;j<4;j++)
@@ -40,7 +42,8 @@ void test_data2()
 			dat=dat+*ptr;			
 			ptr++;
 		}
-		printf("%x  \n",dat);
+		
+		printf("%d 4 Byte Data %x  \n ASCII Converted Data, Base = %d\n",i,dat,base);
 		dest=my_itoa(dest, dat, base);
 		while(dat!=0)
 		{
@@ -48,7 +51,7 @@ void test_data2()
 			dat/=base;
 		}
 		print_memory(dest, l+1);
-		printf("\n");
+		printf("\n Decimal value of given ASCII string: ");
 		dat=my_atoi(dest);
 		printf("%d\n",dat);
 		l=0;
@@ -58,6 +61,7 @@ void test_memory()
 {
 	uint8_t set_2[]= {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
 	uint8_t * ptr = set_2;
+	printf("\n\n\nInitial Data\n");
 	print_memory(ptr,32);
 	printf("\n");
 	my_rev(ptr,12);
@@ -65,6 +69,7 @@ void test_memory()
 	my_memmov(ptr + 25, ptr + 19, 6);
 	my_memzero(ptr + 11,5);
 	my_memmov(ptr,ptr+8,8);
+	printf("Data after manipulations \n");
 	print_memory(ptr,32);
 }	
 		
